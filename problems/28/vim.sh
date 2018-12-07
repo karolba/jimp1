0efa1bbd578627d0e12f0dc48e124bf9ebeb9cf5
Karol Baraniecki <karol@baraniecki.eu>
2018-12-07 13:46:31 +0100

    nvim support
    :te instead of :!

--- problems/28/vim.sh
+++ problems/28/vim.sh
@@ -2,4 +2,8 @@
 if [[ $# == 0 ]]; then
     set -- solution.c
 fi
-exec vim +':map <CR> :!./run.sh<CR>' +'map \<CR> :!./run.sh line<CR>' "$@"
+if vim --version | grep -q '^NVIM'; then
+    exec vim +':map <CR> :w<CR>:te ./run.sh<CR>a' +'map \<CR> :w<CR>:te ./run.sh line<CR>a' "$@"
+else
+    exec vim +':map <CR> :w<CR>:!./run.sh<CR>' +'map \<CR> :w<CR>:!./run.sh line<CR>' "$@"
+fi
